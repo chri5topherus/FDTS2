cb8b6217921517c31cd5cf04667ea8f29b151a1c
Chris <cb@strukt.com>
2016-08-27 15:01:48 +0200

    added DATA_INPUT gameObject

---
+++ Assets/GetPositionData.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GetPositionData : MonoBehaviour {
+
+	public GameObject _DATA_INPUT;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = new Vector3 (_DATA_INPUT.transform.position.x, _DATA_INPUT.transform.position.y, _DATA_INPUT.transform.position.z);
+	}
+}
---
+++ Assets/GetRotationData.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GetRotationData : MonoBehaviour {
+
+	public GameObject _DATA_INPUT;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		this.transform.rotation = _DATA_INPUT.transform.rotation;
+
+	}
+}
